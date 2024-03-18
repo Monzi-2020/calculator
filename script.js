fbd57f66d9d818e8293e0260a4e39efe3da806c5
Monzi2020 <nyenuen@gmail.com>
2024-03-18 18:39:00 +1100

    Declared numbers and operators in javascript. Changed font to kufam Added eventlistner on buttons to get the number and operators

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function divide(a,b){
     return a/b;
 }
 
+let num1 = "";
+let operator;
+let num2 = "";
 
 function operate(num1,operator,num2){
     if(operator === '+') {
@@ -31,3 +34,17 @@ function operate(num1,operator,num2){
         multiply(num1,num2);
     }
 }
+
+const screen = document.querySelector('#screen');
+const numbers = document.querySelectorAll('.button.num')
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+    num1 += number.id;
+    screen.textContent = num1;})})
+
+const opts = document.querySelectorAll('.button.opt')
+opts.forEach((opt) => {
+    opt.addEventListener("click", () => {
+        operator = opt.id;
+    })
+})
